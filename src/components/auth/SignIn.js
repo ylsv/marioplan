8d37663e6ed806d3869ffdb535dce1c58a95a252
yuriy lisov <lisov1988@gmail.com>
2021-04-01 13:09:57 +0300

    add signin and signup components with forms

---
+++ src/components/auth/SignIn.js
@@ -0,0 +1,47 @@
+import {useState} from 'react'
+
+export default function SignIn() {
+  const initialFormState = {email: '', password: ''}
+  const [form, setForm] = useState(initialFormState)
+
+  function handleChange(e) {
+    setForm({...form, [e.target.id]: e.target.value})
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    console.log(form)
+    setForm(initialFormState)
+  }
+
+  return (
+    <div>
+      <div className="container">
+        <form onSubmit={handleSubmit} className="white">
+          <h5 className="grey-text text-darken-3">Sign In</h5>
+          <div className="input-field">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              onChange={handleChange}
+              value={form.email}
+            />
+          </div>
+          <div className="input-field">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              value={form.password}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="input-field">
+            <button className="btn pink lighten-1 z-depth-0">Login</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/auth/SignUp.js
@@ -0,0 +1,70 @@
+import {useState} from 'react'
+
+export default function SignUp() {
+  const initialFormState = {
+    email: '',
+    password: '',
+    firstName: '',
+    lastName: '',
+  }
+  const [form, setForm] = useState(initialFormState)
+
+  function handleChange(e) {
+    setForm({...form, [e.target.id]: e.target.value})
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    console.log(form)
+    setForm(initialFormState)
+  }
+
+  return (
+    <div>
+      <div className="container">
+        <form onSubmit={handleSubmit} className="white">
+          <h5 className="grey-text text-darken-3">Sign Up</h5>
+          <div className="input-field">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              onChange={handleChange}
+              value={form.email}
+            />
+          </div>
+          <div className="input-field">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              value={form.password}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="input-field">
+            <label htmlFor="firstName">First Name</label>
+            <input
+              type="text"
+              id="firstName"
+              onChange={handleChange}
+              value={form.firstName}
+            />
+          </div>
+          <div className="input-field">
+            <label htmlFor="lastName">Last Name</label>
+            <input
+              type="text"
+              id="lastName"
+              onChange={handleChange}
+              value={form.lastName}
+            />
+          </div>
+          <div className="input-field">
+            <button className="btn pink lighten-1 z-depth-0">Sign Up</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}

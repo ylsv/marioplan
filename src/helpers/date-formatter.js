73f8b6a997bd7c924cf9b88e27de53b778a46fb3
yuriy lisov <lisov1988@gmail.com>
2021-04-07 17:31:21 +0300

    minor date fixes

--- src/helpers/date-formatter.js
+++ src/helpers/date-formatter.js
@@ -1,7 +1,10 @@
 export function formatSecondsToDate(seconds) {
   const dateToFormat = new Date(seconds * 1000)
   const formattedDate = new Intl.DateTimeFormat('en', {
+    hour: '2-digit',
+    minute: '2-digit',
     day: '2-digit',
+    hour12: false,
     month: 'long',
     year: 'numeric',
   }).format(dateToFormat)
